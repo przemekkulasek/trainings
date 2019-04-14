a6f44409c7a8063fb07cf04882661b3461c2a895
Przemek Kulasek <przemyslaw.kulasek@hotmail.com>
2019-04-14 20:28:33 +0200

    default literal and digit separator

--- Trainings/Trainings/DataTypes/Numeric.cs
+++ Trainings/Trainings/DataTypes/Numeric.cs
@@ -11,6 +11,7 @@ namespace Trainings.DataTypes
             StringBuilder sb = new StringBuilder();
 
             //signed integers
+            sb.AppendLine();
             sb.AppendLine($"sbyte min:{sbyte.MinValue} max:{sbyte.MaxValue} bits:8");
             sb.AppendLine($"short min:{short.MinValue} max:{short.MaxValue} bits:16");
             sb.AppendLine($"int min:{int.MinValue} max:{int.MaxValue} bits:32");
@@ -27,6 +28,7 @@ namespace Trainings.DataTypes
             sb.AppendLine($"double min:{double.MinValue} max:{double.MaxValue} bits:64");
             sb.AppendLine($"decimal min:{decimal.MinValue} max:{decimal.MaxValue} bits:128");
             sb.AppendLine("double = science, decimal = economy");
+            sb.AppendLine();
 
             return sb.ToString();
         }
--- Trainings/Trainings/Program.cs
+++ Trainings/Trainings/Program.cs
@@ -7,7 +7,23 @@ namespace Trainings
     {
         static void Main()
         {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine($"OS VERSION: {Environment.OSVersion}");
+            Console.WriteLine($"Username: {Environment.UserName}");
+
+            //numeric values
             Console.WriteLine(Numeric.ValuesRange());
+
+            //default literal
+            int defaultLiteral = default;
+            Console.WriteLine($"defaultLiteral: {defaultLiteral}");
+
+            //digit separator
+            Console.WriteLine($"digit separator: {123_456_789L}");
+
+            //108 - pattern matching
 
         }
     }
