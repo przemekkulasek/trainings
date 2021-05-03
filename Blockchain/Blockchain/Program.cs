85e04740555599f8398000a7729c0f25deb0b5de
przemek <przemyslaw.kulasek@hotmail.com>
2021-05-03 16:44:40 +0200

    blockchain

--- Blockchain/Blockchain/Program.cs
+++ Blockchain/Blockchain/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Blockchain.Core;
+using Newtonsoft.Json;
+using System;
 
 namespace Blockchain
 {
@@ -6,7 +8,23 @@ namespace Blockchain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var startTime = DateTime.Now;
+            Core.Blockchain seesharpifyCoin = new();
+            seesharpifyCoin.CreateTransaction(new Transaction("James", "John", 10));
+            seesharpifyCoin.CreateTransaction(new Transaction("John", "seesharpify", 10));
+            seesharpifyCoin.ProcessPendingTransactions("seesharpify");
+            seesharpifyCoin.CreateTransaction(new Transaction("James", "seesharpify", 5));
+            seesharpifyCoin.CreateTransaction(new Transaction("seesharpify", "John", 5));
+            seesharpifyCoin.ProcessPendingTransactions("seesharpify");
+            var endTime = DateTime.Now;
+            Console.WriteLine(JsonConvert.SerializeObject(seesharpifyCoin, Formatting.Indented));
+
+            Console.WriteLine($"Stan konta James: {seesharpifyCoin.GetBalance("James")}");
+            Console.WriteLine($"Stan konta John: {seesharpifyCoin.GetBalance("John")}");
+            Console.WriteLine($"Stan konta seesharpify: {seesharpifyCoin.GetBalance("seesharpify")}");
+
+            Console.WriteLine($"Duration: {endTime - startTime}");
+            Console.ReadKey();
         }
     }
 }
